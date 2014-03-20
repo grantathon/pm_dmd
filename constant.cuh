936dedc9e8be98558d80fac70f06f9434bf8d43b
Praktikant Acc051 <grant.bartel@gmail.com>
2014-03-20 16:28:55 +0100

    Added libfreenect libraries

---
+++ constant.cuh
@@ -0,0 +1,27 @@
+// ###
+// ###
+// ### Practical Course: GPU Programming in Computer Vision
+// ### 
+// ###
+// ### Technical University Munich, Computer Vision Group
+// ### Winter Semester 2013/2014, March 3 - April 4
+// ###
+// ###
+// ### Project Phase: Boundary and Edge Detection using Kinect Depth Images
+// ###
+// ### Group 8
+// ### 
+// ### Xiao HUANG, xiao.huang@tum.de, p071
+// ### Xiao, XUE, xuexiao1989@gmail.com, p072
+// ### Sing Chun, LEE, leesingchun@gmail.com, p077
+// ###
+// ###
+
+#ifndef CONSTANT_H
+#define CONSTANT_H
+
+#define KINECT_SIZE_X 640
+#define KINECT_SIZE_Y 480
+#define PI 3.1415926f
+
+#endif
